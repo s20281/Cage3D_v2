3668992f9015df5fd55a251ac81b0816be92fdb2
Maciek <s20281@pjwstk.edu.pl>
2023-01-15 22:29:32 +0100

    special item tylko dla danego hero

--- Assets/Scripts/Item/SpecialData.cs
+++ Assets/Scripts/Item/SpecialData.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Data/SpecialData")]
 public class SpecialData : ItemData
 {
+    public string heroName;
     public bool agressiveSkill;
     public int heal;
     public int damage;
