b64a1f7ce8c189f09fa5e8855d09340b3d65a457
s20474-kk <s20474@pjwstk.edu.pl>
2023-01-16 01:20:40 +0100

    endStartReader

---
+++ Assets/Scripts/TheEnd.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TheEnd : MonoBehaviour
+{
+    public TMP_Text textLabel;
+    public GameObject readStoryPanel;
+    public ReadObject end1GoodOrStart;
+    public ReadObject end2SoSo;
+    public ReadObject end3TheWorst;
+    public bool isStart;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        readStoryPanel.SetActive(true);
+
+        if (isStart)
+        {
+            if (textLabel != null)
+            {
+                textLabel.text = end1GoodOrStart.Read;
+            }
+        }
+        else
+        {
+
+            GameManager.UIManager.minimapUI.turnOffMinimap();
+
+            if (textLabel != null)
+            {
+                gameObject.SetActive(true);
+
+                if (end1GoodOrStart != null && GameManager.PlayerManager.GetMindPoints() > 20)
+                {
+                    textLabel.text = end1GoodOrStart.Read;
+                }
+                else
+                {
+                    if (end3TheWorst != null && GameManager.PlayerManager.GetMindPoints() < -20)
+                    {
+                        textLabel.text = end3TheWorst.Read;
+                    }
+                    else
+                    {
+                        if (end2SoSo != null && GameManager.PlayerManager.GetMindPoints() > -20 && GameManager.PlayerManager.GetMindPoints() < 20)
+                        {
+                            textLabel.text = end2SoSo.Read;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (readStoryPanel.active && Input.GetKeyDown(KeyCode.Space) && isStart)
+        {
+            readStoryPanel.SetActive(false);
+        }
+    }
+}
